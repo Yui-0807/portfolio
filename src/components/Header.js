7e33488addeddba0302387277f4489c8aa2ba046
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2024-03-06 00:23:09 -0800

    created header nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
+    const scrollToSection = (id) => {
+        const article = document.getElementById(id)
+        if (article) {
+            article.scrollIntoView({ behavior: "smooth" })
+        }
+    }
     return (
-        <>
-           
-        </>
+        
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li onClick={() => scrollToSection('about')}>About</li>
+                <li onClick={() => scrollToSection('projects')}>Projects</li>
+            </ul>
+        </nav>
     )
 }
 
